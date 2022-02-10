015e5bdc7816c806b1abe294d0ba60c7bc98000a
quantumdot <quantumdot@users.noreply.github.com>
2022-02-10 14:09:12 -0500

    refactor label configs - use lxml to create labeling config, rather than straight strings - add support for multianimal DLC projects

--- dlc2labelstudio/label_config.py
+++ dlc2labelstudio/label_config.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+from lxml import etree
 
 import seaborn as sns
 
@@ -13,25 +14,69 @@ def create_label_config(dlc_config: dict, palette: str='bright') -> str:
     Returns:
     str - string containing the label studio labeling configuration.
     '''
-    template = '<View style="display: flex;">\n' \
-             + '    <View style="flex: 90%">\n' \
-             + '        <Image name="image" value="$image" width="750px" maxWidth="1000px" zoom="true" zoomControl="true" brightnessControl="true" contrastControl="true" />\n' \
-             + '    </View>\n' \
-             + '    <View style="flex: 10%; margin-left: 1em">\n' \
-             + '        <Header value="Keypoints" />\n' \
-             + '        <KeyPointLabels name="keypoint-label" toName="image" strokewidth="2" opacity="1" >\n'
+    root = etree.Element('View', style='display: flex;')
+    root.append(create_image_labeling_config())
 
+    labeling_root = etree.SubElement(root, 'View', style='flex: 10%; margin-left: 1em')
+
+    if 'multianimalproject' in dlc_config and dlc_config['multianimalproject']:
+        labeling_root.extend(create_multianimal_bodyparts_labeling_config(dlc_config))
+
+    else:
+        labeling_root.append(etree.Element('Header', value="Bodyparts"))
+        labeling_root.append(create_bodyparts_labeling_config(dlc_config, palette=palette))
+
+
+    return etree.tostring(root, pretty_print=True, encoding="unicode")
+
+
+def create_image_labeling_config(image_name='image', image_value='$image'):
+    root = etree.Element('View', style='flex: 90%')
+    root.append(etree.Element('Image', name=image_name, value=image_value, width="750px", maxWidth="1000px", zoom="true", zoomControl="true", brightnessControl="true", contrastControl="true"))
+    return root
+
+
+def create_bodyparts_labeling_config(dlc_config: dict, palette: str='bright') -> str:
+    kpl = etree.Element('KeyPointLabels', name='keypoint-label', toName='image', strokewidth='2', opacity='1')
     colors = sns.color_palette(palette, len(dlc_config['bodyparts']))
     for bp, color in zip(dlc_config['bodyparts'], colors):
         color = float_rgb_to_int_rgb(color)
+        kpl.append(etree.Element('Label', value=bp, background=rgb_to_hex(color)))
 
-        template += (' ' * 4 * 3) + f'<Label value="{bp}" background="{rgb_to_hex(color)}"/>'
+    return kpl
 
-    template += '        </KeyPointLabels>\n' \
-             +  '    </View>\n' \
-             +  '</View>\n'
 
-    return template
+def create_multianimal_bodyparts_labeling_config(dlc_config: dict, palette: str='bright') -> str:
+    elements = []
+
+    # create labels for individuals, as bounding box annotations
+    indv_labels = etree.Element('RectangleLabels', name='individuals', toName='image', maxUsages='1')
+    colors = sns.color_palette(palette, len(dlc_config['individuals']))
+    for bp, color in zip(dlc_config['individuals'], colors):
+        color = float_rgb_to_int_rgb(color)
+        indv_labels.append(etree.Element('Label', value=bp, background=rgb_to_hex(color)))
+    elements.append(etree.Element('Header', value="Individuals"))
+    elements.append(indv_labels)
+
+    mabp_labels = etree.Element('KeyPointLabels', name='mabp', toName="image")
+    colors = sns.color_palette(palette, len(dlc_config['multianimalbodyparts']))
+    for bp, color in zip(dlc_config['multianimalbodyparts'], colors):
+        color = float_rgb_to_int_rgb(color)
+        mabp_labels.append(etree.Element('Label', value=bp, background=rgb_to_hex(color)))
+    elements.append(etree.Element('Header', value="Multi-Animal Bodyparts"))
+    elements.append(mabp_labels)
+
+    uniqbp_labels = etree.Element('KeyPointLabels', name='uniqbp', toName="image", maxUsages='1')
+    colors = sns.color_palette(palette, len(dlc_config['uniquebodyparts']))
+    for bp, color in zip(dlc_config['uniquebodyparts'], colors):
+        color = float_rgb_to_int_rgb(color)
+        uniqbp_labels.append(etree.Element('Label', value=bp, background=rgb_to_hex(color)))
+    elements.append(etree.Element('Header', value="Unique Bodyparts"))
+    elements.append(uniqbp_labels)
+
+    return elements
+
+
 
 
 def float_rgb_to_int_rgb(color: Tuple[float, float, float]) -> Tuple[int, int, int]:
